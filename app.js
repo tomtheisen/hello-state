49cb1f1a3d0fe79df2b0bdaeabb9bba438963b38
Tom Theisen <ttheisen@cedaron.com>
2023-07-20 10:53:32 -0700

    signatures

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-const fibers = new Set;
+// also faked createElement for bonus points, but don't need to
+function fakeElement(type, props = {}, ...children) {
+    if (children.length) props.children = children;
+    return { $$typeof: Symbol.for('react.element'), ref: null, type, props };
+}
+
 function App() {
     const [count1, setCount1] = React.useState(0);
-    const [count2, setCount2] = fakeState(render, 0);
+    const [count2, setCount2] = fakeState(0);
 
-    const fiber = reactInternals.getCurrentFiber();
-    fibers.add(fiber);
-    console.log(fibers.size, "fibers seen");
+    const br = fakeElement("br");
+    const btn1 = fakeElement("button", { onClick: () => setCount1(c=>c+1) }, count1);
+    const btn2 = fakeElement("button", { onClick: () => setCount2(c=>c+1) }, count2);
 
-    // inlined createElement, but don't need to
-    const btn1 = {
-        $$typeof: Symbol.for('react.element'), 
-        type: "button", 
-        ref: null, 
-        props: { onClick: () => setCount1(c=>c+1), children: count1 },
-    };
-    const btn2 = {
-        $$typeof: Symbol.for('react.element'), 
-        type: "button", 
-        ref: null, 
-        props: { onClick: () => setCount2(c=>c+1), children: count2 },
-    };
-    return [btn1, btn2];
+    // I think the key warning is issued only for certain component types.
+    // This one is going to be "Indeterminate".
+    return ["react state: ", btn1, br, "fake state: ", btn2, br];
 }
 
 const root = ReactDOM.createRoot(rootdiv);
--- fakestate.js
+++ fakestate.js
@@ -24,57 +24,64 @@
     }
 }
 
-/* reactInternals properties:
-    bundleType
-    version
-    rendererPackageName
-    rendererConfig
-    overrideHookState
-    overrideHookStateDeletePath
-    overrideHookStateRenamePath
-    overrideProps
-    overridePropsDeletePath
-    overridePropsRenamePath
-    setErrorHandler
-    setSuspenseHandler
-    scheduleUpdate
-    currentDispatcherRef
-    findHostInstanceByFiber
-    findFiberByHostInstance
-    findHostInstancesForRefresh
-    scheduleRefresh
-    scheduleRoot
-    setRefreshHandler
-    getCurrentFiber
-    reconcilerVersion
-    getLaneLabelMap
-    injectProfilingHooks
-*/
+/** reactInternals properties:
+ *   bundleType
+ *   version
+ *   rendererPackageName
+ *   rendererConfig
+ *   overrideHookState
+ *   overrideHookStateDeletePath
+ *   overrideHookStateRenamePath
+ *   overrideProps
+ *   overridePropsDeletePath
+ *   overridePropsRenamePath
+ *   setErrorHandler
+ *   setSuspenseHandler
+ *   scheduleUpdate
+ *   currentDispatcherRef
+ *   findHostInstanceByFiber
+ *   findFiberByHostInstance
+ *   findHostInstancesForRefresh
+ *   scheduleRefresh
+ *   scheduleRoot
+ *   setRefreshHandler
+ *   getCurrentFiber
+ *   reconcilerVersion
+ *   getLaneLabelMap
+ *   injectProfilingHooks
+ */
 
-const fiberStateMap = new WeakMap;
-// each fiber has two representations, and by the time i get them they're more or less interchangable.
-// i need to arbitrarily pick on of the two to be the key for me weakmap holding state
-// this function will pick one
-function pickKey(fiber) {
-    return fiber.alternate == null || fiberStateMap.has(fiber)
-        ? fiber
-        : fiber.alternate;
-}
-function fakeState(refresh, state) {
-    const fiber = reactInternals.getCurrentFiber();
-    const key = pickKey(fiber);
-    if (fiberStateMap.has(key)) {
-        state = fiberStateMap.get(key);
+const fakeState = (_ => {
+    const fiberStateMap = new WeakMap;
+    // each fiber has two representations, and by the time i get them they're more or less interchangable.
+    // i need to arbitrarily pick on of the two to be the key for me weakmap holding state
+    // this function will pick one
+    function pickKey(fiber) {
+        return fiber.alternate == null || fiberStateMap.has(fiber)
+            ? fiber
+            : fiber.alternate;
     }
-    else {
-        fiberStateMap.set(key, state);
-    }
-    function setState(newState) {
-        if (typeof newState === 'function') {
-            newState = newState(fiberStateMap.get(key));
+    function fakeState(state) {
+        const fiber = reactInternals.getCurrentFiber();
+        const key = pickKey(fiber);
+        if (fiberStateMap.has(key)) {
+            state = fiberStateMap.get(key);
         }
-        fiberStateMap.set(key, newState);
-        refresh();
+        else {
+            fiberStateMap.set(key, state);
+        }
+        function setState(newState) {
+            if (typeof newState === 'function') {
+                newState = newState(fiberStateMap.get(key));
+            }
+            fiberStateMap.set(key, newState);
+
+            // change of props object identity guarantees render won't be suppressed
+            fiber.memoizedProps = { ...fiber.memoizedProps }; 
+            reactInternals.scheduleUpdate(fiber);
+        }
+        return [state, setState];
     }
-    return [state, setState];
-}
+
+    return fakeState;
+})();
